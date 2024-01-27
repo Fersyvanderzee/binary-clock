d2c519452d21cfbcce5c1016fdd4045ae4339f1d
Fersy van der Zee <fersyvanderzee@gmail.com>
2024-01-27 12:49:22 +0100

    Added random time to check.

--- main.py
+++ main.py
@@ -5,27 +5,28 @@ from test import *
 IMG_WIDTH = 1000
 IMG_HEIGHT = 500
 
-time_patterns = {
-    0: [0, 0, 0, 0],
-    1: [0, 0, 1, 0],
-    2: [1, 0, 0, 0],
-    3: [0, 1, 0, 0],
-    4: [0, 0, 0, 1],
-    5: [1, 0, 1, 0],
-    6: [1, 1, 0, 0],
-    7: [0, 1, 0, 1],
-    8: [0, 0, 1, 1],
-    9: [1, 1, 1, 0],
-    10: [1, 1, 0, 1],
-    11: [0, 1, 1, 1]
-}
+MARGIN = 20
 
 image = Image.new('RGB', (IMG_WIDTH, IMG_HEIGHT), '#000000')
-
 draw = ImageDraw.Draw(image)
 
 
 def ellipse_block(xy: tuple, distance: int, circle_width: int, num_in_time: int):
+    time_patterns = {
+        0: [0, 0, 0, 0],
+        1: [0, 0, 1, 0],
+        2: [1, 0, 0, 0],
+        3: [0, 1, 0, 0],
+        4: [0, 0, 0, 1],
+        5: [1, 0, 1, 0],
+        6: [1, 1, 0, 0],
+        7: [0, 1, 0, 1],
+        8: [0, 0, 1, 1],
+        9: [1, 1, 1, 0],
+        10: [1, 1, 0, 1],
+        11: [0, 1, 1, 1]
+    }
+
     if num_in_time > 11:
         num_in_time -= 12
 
@@ -42,6 +43,7 @@ def ellipse_block(xy: tuple, distance: int, circle_width: int, num_in_time: int)
     i = 0
     for render_point in render_points:
         on_off = pattern[i]
+
 
         if on_off == 0:
             fill = None
@@ -60,37 +62,43 @@ def ellipse_block(xy: tuple, distance: int, circle_width: int, num_in_time: int)
         i += 1
 
 
-def convert_time_stamp(time_stamp):
+def convert_time_stamp(time):
     converted_time_stamp = []
-    time_stamp = time_stamp.split(':')
-    hours, minutes = time_stamp
+    time = time.split(':')
+    hours, minutes = time
 
     converted_time_stamp.append(int(hours))
-    for num in minutes:
-        converted_time_stamp.append(int(num))
-
+    for number in minutes:
+        converted_time_stamp.append(int(number))
 
     return converted_time_stamp
 
 
-base_y = IMG_HEIGHT / 2
+def display_time():
+    base_y = IMG_HEIGHT / 2
 
-now = datetime.now()
+    now = datetime.now()
 
-current_time = now.strftime("%H:%M")
+    current_time = now.strftime("%H:%M")
 
-time_stamp = (return_random_time())
-print(time_stamp)
+    random_time = return_random_time()
 
-converted_time = convert_time_stamp(time_stamp=time_stamp)
+    time_stamp = (random_time)
+    print(time_stamp)
 
-coords_x = [200, 600, 800]
+    converted_time = convert_time_stamp(time=time_stamp)
 
-i = 0
-for num in converted_time:
-    ellipse_block((coords_x[i], base_y), 50, 50, num)
-    i += 1
+    coords_x = [200, 600, 800]
+
+    i = 0
+    for num in converted_time:
+        ellipse_block((coords_x[i], base_y), 50, 50, num)
+        i += 1
+
+    draw.rectangle(((MARGIN, MARGIN), (IMG_WIDTH - MARGIN, IMG_HEIGHT - MARGIN)), fill=None, outline='#fff', width=1)
+
+    draw.line(((400, 150), (400, 350)), fill='#fff', width=1)
+    image.save(f'clock.png')
 
 
-draw.line(((400, 150), (400, 350)), fill='#fff', width=1)
-image.show()
+display_time()
--- test.py
+++ test.py
@@ -4,5 +4,7 @@ import random
 def return_random_time():
     hour = random.randint(0, 23)
     minute = random.randint(0, 59)
+    if minute < 10:
+        minute = '0' + str(minute)
     time = f'{hour}:{minute}'
     return time
